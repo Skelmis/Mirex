e0ca0922bc03677cd0c6d949e6dc75a86874037a
skelmis <ethan@koldfusion.xyz>
2023-04-02 22:55:42 +1200

    chore: bump version to 0.1.0

--- mirex/__init__.py
+++ mirex/__init__.py
@@ -1,9 +1,9 @@
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 import logging
 from collections import namedtuple
 from .mirex import Mirex
 
 VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
-version_info = VersionInfo(major=0, minor=0, micro=1, releaselevel="final", serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="final", serial=0)
 logging.getLogger(__name__).addHandler(logging.NullHandler())
